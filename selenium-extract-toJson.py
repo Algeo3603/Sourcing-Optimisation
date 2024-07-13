408b9158cf1d18d877d0a87a0f56d761838157de
Algeo3603 <alangeorge3603@gmail.com>
2024-07-14 03:31:10 +0530

    added jsons for clutch

--- selenium-extract-toJson.py
+++ selenium-extract-toJson.py
@@ -42,8 +42,8 @@ part_dict = {'buyer':{}, 'supplier':{}}
 
 # Open the link of the part to be scraped
 # Future scope -> iterate through links dynamically or read them from a file
-part_link = 'https://www.marklines.com/en/wsw/rear-lamp/'
-part_name = 'Rear Lamp'
+part_link = 'https://www.marklines.com/en/wsw/clutch/'
+part_name = 'Clutch'
 driver.get(part_link)
 print('Navigated to part link')
 
