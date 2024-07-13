72c1ce1916a0369e78c8c8c093dd267598cffd32
Algeo3603 <alangeorge3603@gmail.com>
2024-07-14 03:43:16 +0530

    added jsons for shock absorber part

--- selenium-extract-toJson.py
+++ selenium-extract-toJson.py
@@ -42,8 +42,8 @@ part_dict = {'buyer':{}, 'supplier':{}}
 
 # Open the link of the part to be scraped
 # Future scope -> iterate through links dynamically or read them from a file
-part_link = 'https://www.marklines.com/en/wsw/axle/'
-part_name = 'Axle'
+part_link = 'https://www.marklines.com/en/wsw/shock-absorber/'
+part_name = 'Shock Absorber'
 driver.get(part_link)
 print('Navigated to part link')
 
